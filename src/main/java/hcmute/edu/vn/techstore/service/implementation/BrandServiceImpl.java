83364cf899e1a790abbc85a4973daabcaac9a253
loh <nguyenhahongtuan@gmail.com>
2025-04-09 22:01:17 +0700

    feat: manage Brand

--- src/main/java/hcmute/edu/vn/techstore/service/implementation/BrandServiceImpl.java
+++ src/main/java/hcmute/edu/vn/techstore/service/implementation/BrandServiceImpl.java
@@ -15,6 +15,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
 @Service
 public class BrandServiceImpl implements IBrandService {
     @Autowired
@@ -33,19 +35,16 @@ public class BrandServiceImpl implements IBrandService {
                 .name(brand.getName())
                 .quantity(productRepository.countByBrand_Id(brand.getId()))
                 .sale(orderDetailRepository.countAllByOrder_OrderStatusAndProduct_Brand_Id(EOrderStatus.DELIVERED_SUCCESSFULLY, brand.getId()))
-                .image(brand.getImage().getImagePath())
+                .image(brand.getImage())
                 .build());
     }
 
     @Override
-    public boolean addBrand(BrandRequest brandRequest, MultipartFile file){
+    public boolean addBrand(BrandRequest brandRequest, MultipartFile file) {
         try {
-            Long imageId = imageService.addImage(file);
-            if (imageId == null)
-                return false;
             BrandEntity brand = new BrandEntity();
             brand.setName(brandRequest.getBrandName());
-            brand.setImage(imageService.findById(imageId));
+            brand.setImage(imageService.saveImage(file));
             brandRepository.save(brand);
             return true;
         } catch (Exception e) {
@@ -55,7 +54,80 @@ public class BrandServiceImpl implements IBrandService {
     }
 
     @Override
+    public boolean updateBrand(BrandRequest brandRequest, Long id) {
+        try {
+            BrandEntity brand = brandRepository.findById(id).get();
+            brand.setName(brandRequest.getBrandName());
+            brand.setImage(brandRequest.getBrandImage());
+            brandRepository.save(brand);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateBrand(BrandRequest brandRequest, Long id, MultipartFile file) {
+        try {
+            BrandEntity brand = brandRepository.findById(id).get();
+            brand.setName(brandRequest.getBrandName());
+            brand.setImage(imageService.updateImage(file, brand.getImage()));
+            brandRepository.save(brand);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteBrand(Long brandId){
+        try {
+            BrandEntity brand = brandRepository.findById(brandId).get();
+//            if(!productRepository.findByBrandName(brand.getName()).isEmpty()){
+//                brand.setActive(false);
+//                brandRepository.save(brand);
+//            }
+//            else {
+//                deleteImage(brand.getBrandImg());
+//                brandRepository.delete(brand);
+//            }
+            imageService.deleteImage(brand.getImage());
+            brandRepository.delete(brand);
+            return true;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
     public BrandEntity findByName(String name) {
         return brandRepository.findByName(name);
+    }
+
+    @Override
+    public Optional<BrandEntity> findById(Long id) {
+        return brandRepository.findById(id);
+    }
+
+    @Override
+    public Optional<BrandResponse> findByIdResponse(Long aLong) {
+        return brandRepository.findById(aLong).map(brand -> BrandResponse.builder()
+                .id(brand.getId())
+                .name(brand.getName())
+                .quantity(productRepository.countByBrand_Id(brand.getId()))
+                .sale(orderDetailRepository.countAllByOrder_OrderStatusAndProduct_Brand_Id(EOrderStatus.DELIVERED_SUCCESSFULLY, brand.getId()))
+                .image(brand.getImage())
+                .build());
+    }
+
+    @Override
+    public Optional<BrandRequest> findByIdRequest(Long aLong) {
+        return brandRepository.findById(aLong).map(brand -> BrandRequest.builder()
+                .brandName(brand.getName())
+                .brandImage(brand.getImage())
+                .build());
     }
 }
