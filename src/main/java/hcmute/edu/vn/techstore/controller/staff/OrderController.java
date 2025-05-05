539a0a60506e7b5a2f40c4fedd0cc08f4d3c49a7
HoangDuyen2 <thihoangduyendo@gmail.com>
2025-05-05 16:24:41 +0700

    show order-detail in staff page

--- src/main/java/hcmute/edu/vn/techstore/controller/staff/OrderController.java
+++ src/main/java/hcmute/edu/vn/techstore/controller/staff/OrderController.java
@@ -1,13 +1,13 @@
 package hcmute.edu.vn.techstore.controller.staff;
 
+import hcmute.edu.vn.techstore.Enum.EOrderStatus;
 import hcmute.edu.vn.techstore.dto.response.OrderResponse;
 import hcmute.edu.vn.techstore.service.interfaces.IOrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,9 +25,18 @@ public class OrderController {
     }
 
     @GetMapping("/{id}")
-    public String getOrder(@PathVariable("id") Long id, Model model) {
+    public String getOrder(@PathVariable("id") Long id, Model model,  @RequestParam(name = "status", required = false, defaultValue = "false") boolean status) {
         OrderResponse orderResponse = orderService.getOrderById(id);
+        model.addAttribute("status",status);
         model.addAttribute("orderResponse", orderResponse);
+        model.addAttribute("orderStatus", EOrderStatus.values());
         return "admin/order/order-detail";
+    }
+
+    @PostMapping("/update-status")
+    @ResponseBody
+    public ResponseEntity<?> changeOrderStatus(@RequestBody OrderResponse orderResponse ) {
+        orderService.changeStatusOrder(orderResponse.getId(), orderResponse.getOrderStatus());
+        return ResponseEntity.ok().build();
     }
 }
