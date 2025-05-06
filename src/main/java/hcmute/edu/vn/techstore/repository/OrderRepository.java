2b0be2c577046d898af52e2a10c73c09c8db8ee0
HoangDuyen2 <thihoangduyendo@gmail.com>
2025-05-06 22:28:31 +0700

    Add load order by order status

--- src/main/java/hcmute/edu/vn/techstore/repository/OrderRepository.java
+++ src/main/java/hcmute/edu/vn/techstore/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package hcmute.edu.vn.techstore.repository;
 
+import hcmute.edu.vn.techstore.Enum.EOrderStatus;
 import hcmute.edu.vn.techstore.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,8 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     int countAllByDiscount_Code(String discountCode);
     @Query("select o from OrderEntity o where o.user.account.email = ?1 order by o.orderDate desc ")
     List<OrderEntity> findAllByUserAccount_Email(String email);
+    @Query("select o from OrderEntity o order by o.orderDate desc ")
+    List<OrderEntity> findAllOrderByOrderDateDesc();
+    @Query("select o from OrderEntity o where o.orderStatus = ?1 order by o.orderDate desc ")
+    List<OrderEntity> findAllOrderByOrderStatusAndDateDesc(EOrderStatus status);
 }
