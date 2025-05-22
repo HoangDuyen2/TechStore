daf92df52dd596a1f7556b142b8d24db0d3486b9
loh <nguyenhahongtuan@gmail.com>
2025-05-22 23:56:07 +0700

    feat: refactor report generation by moving DefaultReportStrategy to a separate class

--- src/main/java/hcmute/edu/vn/techstore/service/impl/strategy/ReportContext.java
+++ src/main/java/hcmute/edu/vn/techstore/service/impl/strategy/ReportContext.java
@@ -1,7 +1,6 @@
 package hcmute.edu.vn.techstore.service.impl.strategy;
 
 import hcmute.edu.vn.techstore.dto.response.ReportResponse;
-import hcmute.edu.vn.techstore.service.impl.AccountServiceImpl;
 import hcmute.edu.vn.techstore.service.interfaces.ReportStrategy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,7 +12,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ReportContext {
     private final Map<String, ReportStrategy> strategies;
-    private final AccountServiceImpl.DefaultReportStrategy defaultStrategy;
+    private final DefaultReportStrategy defaultStrategy;
 
     public ReportResponse generateReport(String strategyType, LocalDate startDate, LocalDate endDate) {
         ReportStrategy strategy = strategies.getOrDefault(strategyType, defaultStrategy);
