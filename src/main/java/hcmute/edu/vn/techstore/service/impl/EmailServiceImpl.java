c786537ea26ff859b90f29fd90271ebc3e62f973
Tuan <nguyenhahongtuan@gmail.com>
2025-10-02 17:43:57 +0700

    feat: Add functionality to send discount codes via email

--- src/main/java/hcmute/edu/vn/techstore/service/impl/EmailServiceImpl.java
+++ src/main/java/hcmute/edu/vn/techstore/service/impl/EmailServiceImpl.java
@@ -74,4 +74,30 @@ public class EmailServiceImpl implements IEmailService {
         message.setText(emailContent);
         mailSender.send(message);
     }
+
+    @Override
+    public void sendDiscountCode(String to, String code, String description) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(to);
+        message.setSubject("Mã giảm giá từ TechStore");
+
+        String emailContent = String.format("""
+            Xin chào!
+            
+            Chúng tôi rất vui được gửi đến bạn mã giảm giá đặc biệt từ TechStore.
+            
+            Mã giảm giá của bạn: %s
+            
+            Chi tiết: %s
+            
+            Hãy sử dụng mã này trong lần mua sắm tiếp theo để nhận ưu đãi hấp dẫn!
+            
+            Trân trọng,
+            Đội ngũ TechStore
+            """, code, description);
+
+        message.setText(emailContent);
+        mailSender.send(message);
+    }
 }
--- src/main/java/hcmute/edu/vn/techstore/service/interfaces/IEmailService.java
+++ src/main/java/hcmute/edu/vn/techstore/service/interfaces/IEmailService.java
@@ -3,4 +3,6 @@ package hcmute.edu.vn.techstore.service.interfaces;
 public interface IEmailService {
     void sendVerificationEmail(String to, String verificationToken);
     void sendPasswordResetEmail(String to, String resetToken);
+
+    void sendDiscountCode(String to, String code, String description);
 }
