29f09145cc4b82906bdf3c059e78e2ab5c269f71
HoangDuyen2 <thihoangduyendo@gmail.com>
2025-10-06 12:02:53 +0700

    TLS configuration to change HTTPS.

--- src/main/java/hcmute/edu/vn/techstore/config/SecurityConfig.java
+++ src/main/java/hcmute/edu/vn/techstore/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package hcmute.edu.vn.techstore.config;
 
 import hcmute.edu.vn.techstore.service.impl.UserDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +10,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,7 +31,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(user -> user.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(header ->
+                        header.httpStrictTransportSecurity(hsts ->
+                                hsts.includeSubDomains(true)
+                                        .maxAgeInSeconds(31536000)//1 year
+                                        )
+                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
+                                .contentTypeOptions(contentTypeOptions -> {})
+                                .xssProtection(HeadersConfigurer.XXssConfig::disable))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/register","/uploads/**","/web/assert/**","/forgot-password","/products","/products/**", "/forgot-password","/api/products/search","/about-us")
                         .permitAll()
